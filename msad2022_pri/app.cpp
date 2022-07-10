ddb57976fa315081e2f82a21074d7e886ee07b95
shojiro <shojiro4150@yahoo.co.jp>
2022-07-11 00:44:36 +0900

    slalom5

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -35,6 +35,7 @@ Plotter*        plotter;
 
 BrainTree::BehaviorTree* tr_calibration = nullptr;
 BrainTree::BehaviorTree* tr_run         = nullptr;
+BrainTree::BehaviorTree* tr_slalom      = nullptr;
 BrainTree::BehaviorTree* tr_block       = nullptr;
 State state = ST_INITIAL;
 
@@ -482,6 +483,42 @@ private:
     double srewRate;
 };
 
+class ClimbBoard : public BrainTree::Node { 
+public:
+    ClimbBoard(int direction, int count) : dir(direction), cnt(count) {}
+    Status update() override {
+        curAngle = gyroSensor->getAngle();
+            if(cnt >= 1){
+                leftMotor->setPWM(0);
+                rightMotor->setPWM(0);
+                armMotor->setPWM(-50);
+                cnt++;
+                if(cnt >= 200){
+                    return Status::Success;
+                }
+                return Status::Running;
+            }else{
+                armMotor->setPWM(30);
+                leftMotor->setPWM(23);
+                rightMotor->setPWM(23);
+
+                if(curAngle < -9){
+                    prevAngle = curAngle;
+                }
+                if (prevAngle < -9 && curAngle >= 0){
+                    ++cnt;
+                    _log("ON BOARD");
+                }
+                return Status::Running;
+            }
+    }
+private:
+    int8_t dir;
+    int cnt;
+    int32_t curAngle;
+    int32_t prevAngle;
+};
+
 /*
     usage:
     ".leaf<SetArmPosition>(target_degree, pwm)"
@@ -600,6 +637,7 @@ void main_task(intptr_t unused) {
 
 #if defined(MAKE_RIGHT) /* BEHAVIOR FOR THE RIGHT COURSE STARTS HERE */
     tr_run = nullptr;
+    tr_slalom = nullptr;
     tr_block = nullptr;
 
 #else /* BEHAVIOR FOR THE LEFT COURSE STARTS HERE */
@@ -616,6 +654,45 @@ void main_task(intptr_t unused) {
                 .leaf<IsColorDetected>(CL_BLUE)
             .end()
             .leaf<TraceLine>(SPEED_NORM, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL)
+        .end()
+        .build();
+
+    tr_slalom = (BrainTree::BehaviorTree*) BrainTree::Builder()
+        .composite<BrainTree::ParallelSequence>(1,2)
+            .leaf<IsBackOn>()
+            .composite<BrainTree::MemSequence>()
+                .composite<BrainTree::ParallelSequence>(1,2)
+                        .leaf<IsColorDetected>(CL_BLUE)
+                        .leaf<TraceLine>(SPEED_NORM, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL)
+                .end()
+                .composite<BrainTree::ParallelSequence>(1,2)
+                    .leaf<IsTimeEarned>(1200000)
+                    .leaf<TraceLine>(SPEED_NORM, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL)
+                .end()
+                .composite<BrainTree::ParallelSequence>(1,2)
+                    .leaf<IsTimeEarned>(2600000)
+                    .leaf<TraceLine>(SPEED_SLOW, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL)
+                .end()
+                .composite<BrainTree::ParallelSequence>(1,2)
+                    .leaf<IsTimeEarned>(2400000)
+                    .leaf<RunAsInstructed>(10, 15, 0.0)
+                .end()
+                .composite<BrainTree::ParallelSequence>(1,2)
+                    .leaf<IsTimeEarned>(2500000)
+                    .leaf<RunAsInstructed>(25, 10, 0.0)
+                .end()
+                .composite<BrainTree::ParallelSequence>(1,2)
+                    .leaf<IsTimeEarned>(3000000)
+                    .leaf<RunAsInstructed>(10, 25, 0.0)
+                .end()
+                .composite<BrainTree::ParallelSequence>(1,2)
+                    .leaf<IsTimeEarned>(3000000)
+                    .leaf<RunAsInstructed>(30, 15, 0.0)
+                .end()
+                .composite<BrainTree::ParallelSequence>(1,2)
+                    .leaf<RunAsInstructed>(0, 0, 0.0)
+                .end()
+            .end()
         .end()
         .build();
 
@@ -658,6 +735,7 @@ void main_task(intptr_t unused) {
     /* destroy behavior tree */
     delete tr_block;
     delete tr_run;
+    delete tr_slalom;
     delete tr_calibration;
     /* destroy EV3 objects */
     delete lpf_b;
@@ -703,6 +781,10 @@ void update_task(intptr_t unused) {
             case BrainTree::Node::Status::Success:
                 switch (JUMP) { /* JUMP = 1... is for testing only */
                     case 1:
+                        state = ST_SLALOM;
+                        _log("State changed: ST_CALIBRATION to ST_SLALOM");
+                        break;
+                    case 2:
                         state = ST_BLOCK;
                         _log("State changed: ST_CALIBRATION to ST_BLOCK");
                         break;
@@ -727,11 +809,28 @@ void update_task(intptr_t unused) {
             switch (status) {
             case BrainTree::Node::Status::Success:
                 state = ST_BLOCK;
-                _log("State changed: ST_RUN to ST_BLOCK");
+                _log("State changed: ST_RUN to ST_SLALOM");
                 break;
             case BrainTree::Node::Status::Failure:
                 state = ST_ENDING;
                 _log("State changed: ST_RUN to ST_ENDING");
+                break;
+            default:
+                break;
+            }
+        }
+        break;
+    case ST_SLALOM:
+        if (tr_slalom != nullptr) {
+            status = tr_slalom->update();
+            switch (status) {
+            case BrainTree::Node::Status::Success:
+                state = ST_BLOCK;
+                _log("State changed: ST_SLALOM to ST_BLOCK");
+                break;
+            case BrainTree::Node::Status::Failure:
+                state = ST_ENDING;
+                _log("State changed: ST_SLALOM to ST_ENDING");
                 break;
             default:
                 break;
